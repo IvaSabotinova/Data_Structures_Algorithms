61b4a5faba8518e284819b5e81ed4000d1b57ea4
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2023-07-06 11:38:31 +0300

    Algorithms

---
+++ Algorithms_Introduction/AlgorithmsIntroduction/BubbleSort/Program.cs
@@ -0,0 +1,56 @@
+﻿public class BubbleSort
+{
+    public static void Main()
+    {
+        int[] array = { 5, 2, 8, 1, 3 };
+
+        Console.WriteLine("Array before sorting:");
+        PrintArray(array);
+
+        BubbleSortAlgorithm(array);
+
+        Console.WriteLine("Array after sorting:");
+        PrintArray(array);
+    }
+
+    public static void BubbleSortAlgorithm(int[] array)
+    {
+        int n = array.Length;
+        bool swapped;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    Swap(ref array[j], ref array[j + 1]);
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped in the inner loop,
+            // the array is already sorted, and we can break out of the outer loop.
+            if (!swapped)
+                break;
+        }
+    }
+
+    public static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Algorithms_Introduction/AlgorithmsIntroduction/InsertionSort/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+InsertionSort(numbers);
+
+Console.WriteLine(string.Join(" ", numbers));
+static void InsertionSort(int[] numbers)
+{
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        int j = i;
+        while (j > 0 && numbers[j - 1] > numbers[j])
+        {
+            Swap(numbers, j - 1, j);
+            j--;
+        }
+
+    }
+}
+
+static void Swap(int[] numbers, int firstNumIndex, int secondNumIndex)
+{
+    int temp = numbers[firstNumIndex];
+    numbers[firstNumIndex] = numbers[secondNumIndex];
+    numbers[secondNumIndex] = temp;
+
+}
